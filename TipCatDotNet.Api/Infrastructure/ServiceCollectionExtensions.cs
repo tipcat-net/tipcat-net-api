61703d26e07e28265e51d525b5fdae2932b5f31b
Kirill Taran <kirilltaran@hotmail.com>
2021-10-26 17:34:49 +0300

    Microsoft Graph libraries removed

--- TipCatDotNet.Api/Infrastructure/ServiceCollectionExtensions.cs
+++ TipCatDotNet.Api/Infrastructure/ServiceCollectionExtensions.cs
@@ -5,9 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Graph;
-using Microsoft.Graph.Auth;
-using Microsoft.Identity.Client;
 using TipCatDotNet.Api.Filters.Authorization.HospitalityFacilityPermissions;
 using TipCatDotNet.Api.Options;
 using TipCatDotNet.Api.Services.Auth;
@@ -29,22 +26,6 @@ namespace TipCatDotNet.Api.Infrastructure
 
             return services.AddHttpClient();
         }
-
-
-        public static IServiceCollection AddMicrosoftGraphClient(this IServiceCollection services, IConfiguration configuration)
-            => services.AddTransient(_ =>
-            {
-                var confidentialClientApplication = ConfidentialClientApplicationBuilder
-                    .Create(configuration["AzureB2CUserManagement:AppId"])
-                    .WithTenantId(configuration["AzureB2CUserManagement:TenantId"])
-                    .WithClientSecret(configuration["AzureB2CUserManagement:ClientSecret"])
-                    .Build();
-
-                var clientCredentialProvider = new ClientCredentialProvider(confidentialClientApplication);
-
-                // https://docs.microsoft.com/ru-ru/azure/active-directory-b2c/microsoft-graph-get-started?tabs=app-reg-ga
-                return new GraphServiceClient(clientCredentialProvider);
-            });
 
 
         public static IServiceCollection AddOptions(this IServiceCollection services, IConfiguration configuration, IVaultClient vaultClient)
