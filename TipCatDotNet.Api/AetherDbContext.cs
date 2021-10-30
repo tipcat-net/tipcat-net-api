09966c3a55a8597d49ffa583a0bd3b7a33531b0d
Vasiliy <vasiariabov@mail.ru>
2021-10-30 15:01:48 +0300

    fix drone

---
+++ TipCatDotNet.Api/AetherDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TipCatDotNet.Api.Data.Models.Auth;
+using TipCatDotNet.Api.Data.Models.HospitalityFacility;
+
+namespace TipCatDotNet.Api.Data
+{
+    public class AetherDbContext : DbContext
+    {
+        public AetherDbContext(DbContextOptions<AetherDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        { }
+
+
+        public virtual DbSet<Account> Accounts { get; set; } = null!;
+        public virtual DbSet<Member> Members { get; set; } = null!;
+        public virtual DbSet<Facility> Facilities { get; set; } = null!;
+        public virtual DbSet<MemberInvitation> MemberInvitations { get; set; } = null!;
+    }
+}
