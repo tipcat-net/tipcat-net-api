b33e41aa0e20fb89b64af9e02d813fef7d5ab24f
Kirill Taran <kirilltaran@hotmail.com>
2021-11-07 22:03:49 +0300

    Test fix

--- TipCatDotNet.Api/Models/Auth/Validators/MemberInvitationValidator.cs
+++ TipCatDotNet.Api/Models/Auth/Validators/MemberInvitationValidator.cs
@@ -1,19 +1,23 @@
 ﻿using FluentValidation;
 using FluentValidation.Results;
 using TipCatDotNet.Api.Data.Models.Auth;
+using TipCatDotNet.Api.Models.Auth.Enums;
 
 namespace TipCatDotNet.Api.Models.Auth.Validators
 {
     public class MemberInvitationValidator : AbstractValidator<MemberInvitation>
     {
-        public new ValidationResult Validate(MemberInvitation invitation)
+        public new ValidationResult Validate(MemberInvitation? invitation)
         {
-            RuleFor(x => x)
-                .NotNull()
-                .WithMessage("No invitations found for that member.");
+            if (invitation is null)
+                return new ValidationResult(new[] { new ValidationFailure(nameof(invitation), "No invitations found for that member") });
 
             RuleFor(x => x)
                 .NotEmpty()
+                .WithMessage("No invitations found for that member.");
+
+            RuleFor(x => x!.State)
+                .NotEqual(InvitationStates.Accepted)
                 .WithMessage("The invitation was accepted already.");
 
             return base.Validate(invitation);
