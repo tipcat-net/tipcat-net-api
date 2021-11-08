99b7b84408011f4acd20e2511ecad7e79b11a4ff
Kirill Taran <kirilltaran@hotmail.com>
2021-11-08 19:21:08 +0300

    Member invitation states display in a MemberResponse

--- TipCatDotNet.Api/Services/Auth/IInvitationService.cs
+++ TipCatDotNet.Api/Services/Auth/IInvitationService.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
+using TipCatDotNet.Api.Models.Auth.Enums;
 using TipCatDotNet.Api.Models.HospitalityFacilities;
 
 namespace TipCatDotNet.Api.Services.Auth
@@ -8,6 +10,8 @@ namespace TipCatDotNet.Api.Services.Auth
     public interface IInvitationService
     {
         Task<Result> CreateAndSend(MemberRequest request, CancellationToken cancellationToken = default);
+
+        Task<Dictionary<int, InvitationStates>> GetState(IEnumerable<int> accountId, CancellationToken cancellationToken = bad);
 
         Task<Result> Redeem(int memberId, CancellationToken cancellationToken = default);
 
