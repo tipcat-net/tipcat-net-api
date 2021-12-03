ee9d913d228a1f1fbbed58d0f3ce7f2c2165da37
Daniyar Askarov <daniyar.ozat.97@outlook..com>
2021-12-03 18:51:51 +0600

    Migration was added

---
+++ TipCatDotNet.Api/Migrations/20211203125055_PaymentClientMessagePropertyAdded.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TipCatDotNet.Api.Migrations
+{
+    public partial class PaymentClientMessagePropertyAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "Transactions",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Transactions");
+        }
+    }
+}
