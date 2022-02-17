a0339031e10abaf7a399253ebd15633811cd4c19
Kirill Taran <kirilltaran@hotmail.com>
2022-02-17 16:54:00 +0300

    Migration added

---
+++ TipCatDotNet.Api/Migrations/20220217135323_ApplicationPreferences.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TipCatDotNet.Api.Data.Models.HospitalityFacility;
+
+#nullable disable
+
+namespace TipCatDotNet.Api.Migrations
+{
+    public partial class ApplicationPreferences : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationPreferences",
+                table: "Members",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "{}");
+
+            migrationBuilder.AddColumn<AccountPreferences>(
+                name: "Preferences",
+                table: "Accounts",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationPreferences",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "Preferences",
+                table: "Accounts");
+        }
+    }
+}
