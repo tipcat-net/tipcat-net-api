8e973bf847ad1b692eb48f7d5478022fc971ff31
Daniyar Askarov <daniyar.ozat.97@outlook..com>
2022-02-18 21:43:36 +0600

    Migration AccountCurrencyWasAded

--- TipCatDotNet.Api/Migrations/20220218154127_AccountCurrencyWasAdded.Designer.cs
+++ TipCatDotNet.Api/Migrations/20220218154127_AccountCurrencyWasAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TipCatDotNet.Api.Data;
@@ -12,9 +13,10 @@ using TipCatDotNet.Api.Data.Models.HospitalityFacility;
 namespace TipCatDotNet.Api.Migrations
 {
     [DbContext(typeof(AetherDbContext))]
-    partial class AetherDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220218154127_AccountCurrencyWasAdded")]
+    partial class AccountCurrencyWasAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TipCatDotNet.Api/Migrations/20220218154127_AccountCurrencyWasAdded.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TipCatDotNet.Api.Migrations
+{
+    public partial class AccountCurrencyWasAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Accounts",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Accounts");
+        }
+    }
+}
